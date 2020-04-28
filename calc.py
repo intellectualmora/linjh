4be976f6a7d09b4eccfcf2231da3d910b5b94d33
intellectualmora <1105191449@qq.com>
2020-04-28 22:49:18 +0800

    update

--- calc.py
+++ calc.py
@@ -1,4 +1,7 @@
-import numpy
-import matplotlib
-import pandas
+# -*- coding: utf-8 -*-
+# @Time : 2020/4/28 22:47
+# @Author : mora
+# @FileName: calc.py
+# @Software: PyCharm
+# @Github ：https://github.com/intellectualmora/linjh
 
